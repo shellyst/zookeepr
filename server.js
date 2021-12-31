f6737d3673022397ed346450d70cd4ce6fc696b0
Michelle <mchristine.stone91@gmail.com>
2021-12-31 00:29:41 -0500

    fix heroku error

--- server.js
+++ server.js
@@ -42,13 +42,13 @@ function findById(id, animalsArray) {
   return result;
 }
 
-app.get("/api/animals", (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
+// app.get("/api/animals", (req, res) => {
+//   let results = animals;
+//   if (req.query) {
+//     results = filterByQuery(req.query, results);
+//   }
+//   res.json(results);
+// });
 
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
